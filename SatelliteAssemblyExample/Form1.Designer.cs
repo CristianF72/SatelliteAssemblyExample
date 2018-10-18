f24859667801e0030dcb46b5ecf22fa7bc295692
CristianF72 <cristian.frent@gmail.com>
2018-10-18 13:56:58 +0300

    Satellite assembly example created

---
+++ SatelliteAssemblyExample/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SatelliteAssemblyExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbLanguage = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMultiLangResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbLanguage
+            // 
+            this.cmbLanguage.FormattingEnabled = true;
+            this.cmbLanguage.Location = new System.Drawing.Point(173, 31);
+            this.cmbLanguage.Name = "cmbLanguage";
+            this.cmbLanguage.Size = new System.Drawing.Size(121, 21);
+            this.cmbLanguage.TabIndex = 0;
+            this.cmbLanguage.SelectedIndexChanged += new System.EventHandler(this.cmbLanguage_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Language";
+            // 
+            // lblMultiLangResult
+            // 
+            this.lblMultiLangResult.AutoSize = true;
+            this.lblMultiLangResult.Location = new System.Drawing.Point(185, 91);
+            this.lblMultiLangResult.Name = "lblMultiLangResult";
+            this.lblMultiLangResult.Size = new System.Drawing.Size(93, 13);
+            this.lblMultiLangResult.TabIndex = 2;
+            this.lblMultiLangResult.Text = "lblMultiLangResult";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 167);
+            this.Controls.Add(this.lblMultiLangResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbLanguage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbLanguage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMultiLangResult;
+    }
+}
+
---
+++ SatelliteAssemblyExample/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SatelliteAssemblyExample
+{
+    //https://www.codeproject.com/Articles/352105/Satellite-Assembly-Example-in-Csharp-Step-by-Step
+    public partial class Form1 : Form
+    {
+        readonly System.Resources.ResourceManager _resourceManager = new System.Resources.ResourceManager("SatelliteAssemblyExample.string", Assembly.GetExecutingAssembly());
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeCulture(string sLangCode)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(sLangCode);
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(sLangCode);
+            lblMultiLangResult.Text = _resourceManager.GetString("lblResult");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbLanguage.Items.Add("Ingliș");
+            cmbLanguage.Items.Add("Franțuzăște");
+            cmbLanguage.Items.Add("Hitaliană");
+            cmbLanguage.Items.Add("Rusăște");
+            cmbLanguage.Items.Add("Iapaneză");
+            ChangeCulture("en-US");
+        }
+
+        private void cmbLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sLangCode;
+            if (cmbLanguage.SelectedIndex == 0)
+            {
+                sLangCode = "en-US";
+                ChangeCulture(sLangCode);
+            }
+            else if (cmbLanguage.SelectedIndex == 1)
+            {
+                sLangCode = "fr-FR";
+                ChangeCulture(sLangCode);
+            }
+            else if (cmbLanguage.SelectedIndex == 2)
+            {
+                sLangCode = "it";
+                ChangeCulture(sLangCode);
+            }
+            else if (cmbLanguage.SelectedIndex == 3)
+            {
+                sLangCode = "ru";
+                ChangeCulture(sLangCode);
+            }
+            else if (cmbLanguage.SelectedIndex == 4)
+            {
+                sLangCode = "ja";
+                ChangeCulture(sLangCode);
+            }
+        }
+    }
+}
